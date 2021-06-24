76119e400c826146f1a47293ad65d52339887032
Lav Chaudhary <lav.chaudhary5@gmail.com>
2021-06-25 04:51:26 +0530

    Update game.py

--- game.py
+++ game.py
@@ -6,26 +6,6 @@ from numpy import *
 import os
 from colorama import Fore
 
-"""
-THESE ARE THE RULES FOR JOHN CONOR'S GAME OF LIFE
-
-The universe of the Game of Life is an infinite, two-dimensional orthogonal grid of square cells, 
-each of which is in one of two possible states, live or dead, (or populated and unpopulated, respectively). 
-Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent. 
-At each step in time, the following transitions occur:
-    Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-    Any live cell with two or three live neighbours lives on to the next generation.
-    Any live cell with more than three live neighbours dies, as if by overpopulation.
-    Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
-These rules, which compare the behavior of the automaton to real life, can be condensed into the following:
-    Any live cell with two or three live neighbours survives.
-    Any dead cell with three live neighbours becomes a live cell.
-    All other live cells die in the next generation. Similarly, all other dead cells stay dead.
-
-The initial pattern constitutes the seed of the system. The first generation is created by applying the above rules simultaneously to every cell in the seed, live or dead; births and deaths occur simultaneously, and the discrete moment at which this happens is sometimes called a tick. Each generation is a pure function of the preceding one. The rules continue to be applied repeatedly to create further generations.
-"""
-
 def printGrid(iteration, list_of_lists, m, n):
     os.system('clear')
     print(Fore.BLUE + '\nIteration : '+str(iteration)+'\n')
@@ -160,10 +140,3 @@ def main():
     print('Starting population : '+str(StartingPopulation))
     print('Current population : '+str(LatestPopulation))
 main()
-
-"""
-Some observations:
-Constant after some iterations : python3 game.py 25x50 12 25 50
-Constant after a single iteration : python3 game.py 25x50 12 29 50
-
-"""
